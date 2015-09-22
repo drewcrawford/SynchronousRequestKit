09a4367bba4c2f0fdea18063577d7514c5d11674
Drew Crawford <drew@sealedabstract.com>
2015-09-22 01:15:47 -0500

    Initial Commit

---
+++ SynchronousRequestKit/SynchronousRequestKit.h
@@ -0,0 +1,19 @@
+//
+//  SynchronousRequestKit.h
+//  SynchronousRequestKit
+//
+//  Created by Drew Crawford on 9/22/15.
+//  Copyright © 2015 DrewCrawfordApps. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SynchronousRequestKit.
+FOUNDATION_EXPORT double SynchronousRequestKitVersionNumber;
+
+//! Project version string for SynchronousRequestKit.
+FOUNDATION_EXPORT const unsigned char SynchronousRequestKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SynchronousRequestKit/PublicHeader.h>
+
+
