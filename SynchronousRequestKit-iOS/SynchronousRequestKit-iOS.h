c718e3dcbcbe45075f6b1e04fc26c761db7cb4ea
Drew Crawford <drew@sealedabstract.com>
2015-12-16 02:44:44 -0600

    iOS port; see #1

---
+++ SynchronousRequestKit-iOS/SynchronousRequestKit-iOS.h
@@ -0,0 +1,19 @@
+//
+//  SynchronousRequestKit-iOS.h
+//  SynchronousRequestKit-iOS
+//
+//  Created by Drew Crawford on 12/16/15.
+//  Copyright © 2015 DrewCrawfordApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SynchronousRequestKit-iOS.
+FOUNDATION_EXPORT double SynchronousRequestKit_iOSVersionNumber;
+
+//! Project version string for SynchronousRequestKit-iOS.
+FOUNDATION_EXPORT const unsigned char SynchronousRequestKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SynchronousRequestKit_iOS/PublicHeader.h>
+
+
