fb7816d9a5259fe5782b8c17926681e3fda7523a
Drew Crawford <drew@sealedabstract.com>
2016-10-09 22:27:10 -0500

    Upgrade to Swift 3

--- SynchronousRequestKit/NSURLSession+Synchronous.swift
+++ SynchronousRequestKit/NSURLSession+Synchronous.swift
@@ -13,40 +13,40 @@ import Foundation
 private var cassettes : [SynchronousDataTask] = []
 
 /**Add a cassette to be played back on the next request. This is primarily used for testing purposes.*/
-public func addCassette(string string: String) {
-    let response = NSHTTPURLResponse(URL: NSURL(), statusCode: 200, HTTPVersion: nil, headerFields: nil)
-    let cassette = SynchronousDataTask(task: NSURLSessionDataTask(), error: nil, data: string.dataUsingEncoding(NSUTF8StringEncoding), response:response)
+public func addCassette(string: String) {
+    let response = HTTPURLResponse(url: URL(string: "http://www.example.com/")!, statusCode: 200, httpVersion: nil, headerFields: nil)
+    let cassette = SynchronousDataTask(task: URLSessionDataTask(), error: nil, data: string.data(using: String.Encoding.utf8), response:response)
     cassettes.append(cassette)
 }
 
-public func addCassette(path path: String, testClass: AnyClass) {
-    let path = NSBundle(forClass: testClass).pathForResource(path, ofType: "cassette")
-    let data = NSData(contentsOfFile: path!)!
-    let str = NSString(data: data, encoding: NSUTF8StringEncoding)
+public func addCassette(path: String, testClass: AnyClass) {
+    let path = Bundle(for: testClass).path(forResource: path, ofType: "cassette")
+    let data = try! Data(contentsOf: URL(fileURLWithPath: path!))
+    let str = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
     addCassette(string: str as! String)
 }
 
 
-enum SynchronousDataTaskError : ErrorType {
-    case NoData
-    case BadHTTPStatus(code: Int, body: String?)
-    case NotHTTPResponse
-    case NoResponse
-    case NotUTF8
-    case NotJSONDict
+enum SynchronousDataTaskError : Error {
+    case noData
+    case badHTTPStatus(code: Int, body: String?)
+    case notHTTPResponse
+    case noResponse
+    case notUTF8
+    case notJSONDict
 }
 
 import Foundation
 private let errorDomain = "SynchronousDataTaskErrorDomain"
 public final class SynchronousDataTask {
-    public var task: NSURLSessionDataTask
+    public var task: URLSessionDataTask
     
     /**The error, if any, returned by Cocoa.  If you want a better error, try errorOrData/errorOrString. */
     public var underlyingCocoaError: NSError?
-    public var underlyingCocoaData: NSData?
-    public var underlyingCocoaResponse: NSURLResponse?
+    public var underlyingCocoaData: Data?
+    public var underlyingCocoaResponse: URLResponse?
     
-    private init(task: NSURLSessionDataTask, error: NSError?, data: NSData?, response:NSURLResponse?) {
+    fileprivate init(task: URLSessionDataTask, error: NSError?, data: Data?, response:URLResponse?) {
         self.underlyingCocoaData = data
         self.underlyingCocoaError = error
         self.underlyingCocoaResponse = response
@@ -58,22 +58,22 @@ public final class SynchronousDataTask {
     This function tries to get some data for the request.
     
     */
-    public func getData() throws -> NSData {
+    public func getData() throws -> Data {
         if let cocoaError = underlyingCocoaError { //may have an error already
             throw cocoaError
         }
         if underlyingCocoaResponse == nil {
-            throw SynchronousDataTaskError.NoResponse
+            throw SynchronousDataTaskError.noResponse
         }
-        guard let myResponse = underlyingCocoaResponse as? NSHTTPURLResponse else {
-            throw SynchronousDataTaskError.NotHTTPResponse
+        guard let myResponse = underlyingCocoaResponse as? HTTPURLResponse else {
+            throw SynchronousDataTaskError.notHTTPResponse
             
         }
         if myResponse.statusCode < 200 || myResponse.statusCode >= 300 {
-            throw SynchronousDataTaskError.BadHTTPStatus(code: myResponse.statusCode, body: _string)
+            throw SynchronousDataTaskError.badHTTPStatus(code: myResponse.statusCode, body: _string)
         }
         guard let d = underlyingCocoaData else {
-            throw SynchronousDataTaskError.NoData
+            throw SynchronousDataTaskError.noData
         }
         return d
     }
@@ -83,7 +83,7 @@ public final class SynchronousDataTask {
     var _string : String? {
         get {
             guard let data = underlyingCocoaData else { return nil }
-            guard let str = NSString(data: data, encoding: NSUTF8StringEncoding) else { return nil }
+            guard let str = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { return nil }
             return str as String
         }
     }
@@ -94,57 +94,57 @@ public final class SynchronousDataTask {
     */
     public func getString() throws -> String {
         let data = try self.getData()
-        guard let str = NSString(data: data, encoding: NSUTF8StringEncoding) else { throw SynchronousDataTaskError.NotUTF8 }
+        guard let str = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { throw SynchronousDataTaskError.notUTF8 }
         return str as String
     }
     
     /**This function tries to get a JSON dictionary for the request. */
     public func getJsonDict() throws -> [String: AnyObject] {
         let data = try getData()
-        let jsonObj = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions())
+        let jsonObj = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions())
         if let j = jsonObj as? [String: AnyObject] {
             return j
         }
-        throw SynchronousDataTaskError.NotJSONDict
+        throw SynchronousDataTaskError.notJSONDict
     }
     
     /**This checks the response to see if it's a well-known string. */
-    public func isParticularError(code code: Int, body: String) -> Bool {
-        guard let myResponse = underlyingCocoaResponse as? NSHTTPURLResponse else { return false }
+    public func isParticularError(code: Int, body: String) -> Bool {
+        guard let myResponse = underlyingCocoaResponse as? HTTPURLResponse else { return false }
         if myResponse.statusCode != code {
             return false
         }
         guard let u = underlyingCocoaData else {
             return false
         }
-        let str = NSString(data: u, encoding: NSUTF8StringEncoding)
+        let str = NSString(data: u, encoding: String.Encoding.utf8.rawValue)
         guard let s = str else { return false }
-        if s != body {
+        if s as String != body {
             return false
         }
         return true
     }
 }
-extension NSURLSession {
+extension URLSession {
     
-    public func synchronousDataRequestWithRequest(request: NSURLRequest) -> SynchronousDataTask {
+    public func synchronousDataRequestWithRequest(_ request: URLRequest) -> SynchronousDataTask {
         if cassettes.count > 0 {
             return cassettes.removeFirst()
         }
         
-        let sema = dispatch_semaphore_create(0)
-        var data : NSData?
-        var response : NSURLResponse?
+        let sema = DispatchSemaphore(value: 0)
+        var data : Data?
+        var response : URLResponse?
         var error : NSError?
-        let task = self.dataTaskWithRequest(request) { (ldata, lresponse, lerror) -> Void in
+        let task = self.dataTask(with: request, completionHandler: { (ldata, lresponse, lerror) -> Void in
             data = ldata
             response = lresponse
-            error = lerror
-            dispatch_semaphore_signal(sema);
+            error = lerror as NSError?
+            sema.signal();
             return;
-        }
+        }) 
         task.resume()
-        dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER)
+        sema.wait()
         return SynchronousDataTask(task: task, error: error, data: data, response: response)
     }
 }
